6b3f654f3c700195cfb340084ab981c74853645a
sergr972 <sergr972@gmail.com>
2023-10-08 20:49:55 +0300

    Displaying food list in JSP

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,23 +1,36 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.FoodList;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
+import static ru.javawebinar.topjava.util.FoodList.CALORIES_PER_DAY;
 
 public class MealServlet extends HttpServlet {
 
     private static final Logger log = getLogger(MealServlet.class);
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("redirect to meals");
-
-        response.sendRedirect("meals.jsp");
+        log.debug("forward to meals");
+        List<MealTo> mealToList = MealsUtil
+                .filteredByStreams(FoodList.getMealList()
+                        , LocalTime.of(0, 0), LocalTime.of(23, 59)
+                        , CALORIES_PER_DAY);
+        request.setAttribute("mealToList", mealToList);
+        request.setAttribute("formatter", formatter);
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,5 +1,24 @@
-<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
+<style>
+    table, td, th {
+        border: 2px solid black;
+    }
+
+    table {
+        border-collapse: collapse;
+    }
+
+    th {
+        font-weight: bold;
+        padding: 12px;
+    }
+
+    td {
+        padding: 12px;
+    }
+</style>
 <head>
     <title>Meals</title>
 </head>
@@ -7,5 +26,26 @@
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+<br>
+<jsp:useBean id="mealToList" scope="request" type="java.util.List<ru.javawebinar.topjava.model.MealTo>"/>
+<jsp:useBean id="formatter" scope="request" type="java.time.format.DateTimeFormatter"/>
+<table>
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+    </tr>
+    <c:forEach var="mealTo" items="${mealToList}">
+        <tr>
+            <c:set var="td_color" scope="session" value="#008000FF"/>
+            <c:if test="${mealTo.excess == true}">
+                <c:set var="td_color" scope="session" value="#FF0000"/>
+            </c:if>
+            <td style="color:${td_color};"><c:out value="${mealTo.dateTime.format(formatter)}"/></td>
+            <td style="color:${td_color};"><c:out value="${mealTo.description}"/></td>
+            <td style="color:${td_color};"><c:out value="${mealTo.calories}"/></td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
