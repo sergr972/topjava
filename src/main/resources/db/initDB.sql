88c53dbccfade0895950c06a37bad900bb7f8a0a
sergr972 <sergr972@gmail.com>
2023-10-25 22:58:35 +0300

    MealServiceTest + MealTestData + optional

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -28,7 +28,7 @@ CREATE TABLE user_role
 CREATE TABLE meals
 (
     id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-    dateTime        TIMESTAMP    NOT NULL,
+    date_time       TIMESTAMP    NOT NULL,
     description     VARCHAR      NOT NULL,
     calories        VARCHAR      NOT NULL,
     user_id         INTEGER      NOT NULL,
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,88 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.Assert.assertThrows;
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceTest {
+
+    @Autowired
+    private MealService service;
+
+    @Test
+    public void get() {
+        Meal actual = service.get(meal1.getId(), USER_ID);
+        assertMatch(actual, meal1);
+    }
+
+    @Test
+    public void delete() {
+        service.delete(meal1.getId(), USER_ID);
+        assertThrows(NotFoundException.class, () -> service.get(meal1.getId(), USER_ID));
+    }
+
+    @Test
+    public void getBetweenInclusive() {
+        assertMatch(service.getBetweenInclusive(
+                        LocalDate.of(2020, Month.JANUARY, 31),
+                        LocalDate.of(2020, Month.JANUARY, 31), USER_ID),
+                meal7, meal6, meal5, meal4);
+    }
+
+    @Test
+    public void getAll() {
+        assertMatch(service.getAll(USER_ID), meals);
+    }
+
+    @Test
+    public void update() {
+        Meal updated = getUpdated();
+        service.update(updated, ADMIN_ID);
+        assertMatch(service.get(adminMeal1.getId(), ADMIN_ID), updated);
+    }
+
+    @Test
+    public void create() {
+        Meal created = service.create(getNew(), USER_ID);
+        Integer newId = created.getId();
+        Meal newMeal = getNew();
+        newMeal.setId(newId);
+        assertMatch(created, newMeal);
+        assertMatch(service.get(newId, USER_ID), newMeal);
+    }
+
+    @Test
+    public void deleteWrongId() {
+        assertThrows(NotFoundException.class, () -> service.delete(NOT_FOUND, USER_ID));
+    }
+
+    @Test
+    public void getNotFound() {
+        assertThrows(NotFoundException.class, () -> service.get(NOT_FOUND, USER_ID));
+    }
+
+    @Test
+    public void updateInvalidAuthorization() {
+        assertThrows(NotFoundException.class, () -> service.update(meal1, ADMIN_ID));
+    }
+}
