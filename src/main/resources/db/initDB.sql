95e84a04868a207594263ecfa042de8e39ae65c2
sergr972 <sergr972@gmail.com>
2023-10-25 15:13:04 +0300

    Revert "Create and populate meals table"
    This reverts commit 803cdc87b394f28fa0657286c8c88184bbaf7051.

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -23,14 +23,3 @@ CREATE TABLE user_role
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
-
-CREATE TABLE meals
-(
-    id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-    dateTime        TIMESTAMP    NOT NULL,
-    description     VARCHAR      NOT NULL,
-    calories        VARCHAR      NOT NULL,
-    user_id         INTEGER      NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
-CREATE UNIQUE INDEX meals_unique_user_datetime ON meals (user_id, dateTime );
