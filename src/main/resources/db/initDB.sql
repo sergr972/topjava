ff173b9fb5954c23c67e3275d3fe4f02a22f250e
sergr972 <sergr972@gmail.com>
2023-10-26 18:49:24 +0300

    update script DB

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -30,8 +30,8 @@ CREATE TABLE meals
     id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
     date_time       TIMESTAMP    NOT NULL,
     description     VARCHAR      NOT NULL,
-    calories        VARCHAR      NOT NULL,
+    calories        INTEGER      NOT NULL,
     user_id         INTEGER      NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
-CREATE UNIQUE INDEX meals_unique_user_datetime ON meals (user_id, dateTime );
+CREATE UNIQUE INDEX meals_unique_user_datetime ON meals (user_id, date_time );
