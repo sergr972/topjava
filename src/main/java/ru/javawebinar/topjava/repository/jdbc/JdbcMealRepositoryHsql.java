301d544a38aed897fb2bee5830721d16b56eadc5
sergr972 <sergr972@gmail.com>
2023-11-07 16:56:25 +0300

    optional6_jdbc_profiles_in_work

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryHsql.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryHsql.java
@@ -8,23 +8,23 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-@Profile(Profiles.POSTGRES_DB)
-public class JdbcMealRepository extends AbstractJdbcMealRepository {
+@Profile(Profiles.HSQL_DB)
+public class JdbcMealRepositoryHsql extends AbstractJdbcMealRepository {
 
     @Autowired
-    public JdbcMealRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-        super(jdbcTemplate,
-                namedParameterJdbcTemplate);
+    public JdbcMealRepositoryHsql(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
         return jdbcTemplate.query(
                 "SELECT * FROM meal WHERE user_id=?  AND date_time >=  ? AND date_time < ? ORDER BY date_time DESC",
-                ROW_MAPPER, userId, startDateTime, endDateTime);
+                ROW_MAPPER, userId, Timestamp.valueOf(startDateTime), Timestamp.valueOf(endDateTime));
     }
 }
